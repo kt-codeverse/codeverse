6accc444fd8f9a3092af260f87a3522e648fd9da
youminki <dbalsrl7648@gmail.com>
2025-11-17 16:15:39 +0900

    fix : 사용자 회원가입 필드에 전화번호, 닉네임 필드 추가

--- backend/src/modules/user/dto/create-user.dto.ts
+++ backend/src/modules/user/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  MinLength,
+  IsOptional,
+  Matches,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
@@ -11,9 +17,19 @@ export class CreateUserDto {
   @MinLength(6)
   password: string;
 
-  @ApiProperty({ required: false })
-  @IsOptional()
-  name?: string;
+  @ApiProperty({ minLength: 6 })
+  @IsNotEmpty()
+  @MinLength(6)
+  passwordConfirm: string;
+
+  @ApiProperty({ example: '사용자123' })
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ example: '+821012341234' })
+  @IsNotEmpty()
+  @Matches(/^[+0-9\-\s]+$/)
+  phoneNumber: string;
 
   @ApiProperty({ required: false })
   @IsOptional()
