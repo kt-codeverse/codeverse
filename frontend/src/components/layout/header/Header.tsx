08b1d50a43509092f7474a8a8f181d96ebeb75cf
youserstack <163319030+youserstack@users.noreply.github.com>
2025-11-18 15:13:04 +0900

    [FIX] 헤더수정 및 린트수정

--- frontend/src/components/layout/header/Header.tsx
+++ frontend/src/components/layout/header/Header.tsx
@@ -12,10 +12,10 @@ import HeaderHostingButton from './HeaderHostingButton';
 
 export default function Header() {
   const pathname = usePathname();
-  const isSigninPage = pathname === '/signin';
-  const isSignupPage = pathname === '/signup';
 
-  if (isSigninPage || isSignupPage) return null;
+  const hiddenPages = ['/signin', '/signup', '/hosting'];
+  const isHiddenPage = hiddenPages.some((page) => pathname.startsWith(page));
+  if (isHiddenPage) return null;
 
   return (
     <header className="sticky top-0 z-50 bg-white shadow-md p-4 md:px-10 pt-4 pb-8 space-y-4 whitespace-nowrap">
@@ -23,8 +23,8 @@ export default function Header() {
       <section className="max-w-7xl mx-auto grid grid-cols-3 items-center border border-dashed">
         {/* 좌측 */}
         <div className="relative flex items-center h-10 cursor-pointer my-auto shrink-0">
-          <span className="text-2xl font-bold text-[#FF385C] ml-2 hidden md:inline-block ">
-            TripNest
+          <span className="text-2xl font-bold text-[#FF385C] ml-2 hidden md:inline-block uppercase">
+            tripnest
           </span>
         </div>
 
